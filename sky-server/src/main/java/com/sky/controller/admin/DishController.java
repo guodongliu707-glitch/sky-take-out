76dec2f4c67cb1cc8fce0e8206df6c04c7affbb6
liu <3195223408707@gamil>
2026-04-25 19:02:16 +0900

    味わい

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,4 +1,26 @@
 package com.sky.controller.admin;
 
+import com.sky.entity.Dish;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/dish")
+@Slf4j
 public class DishController {
+    @Autowired
+    private DishService dishService;
+
+    @PostMapping
+    public Result save(@RequestBody Dish dish) {
+        log.info("save dish:{}", dish);
+        dishService.add(dish);
+        return Result.success();
+    }
 }
