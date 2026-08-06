388d7c1d5d013d66030f2fd74668917fabb95797
guodongliu707-glitch <guodongliu707@gmail.com>
2026-08-07 08:40:52 +0900

    版本恢复

---
+++ sky-server/src/main/java/com/sky/controller/admin/son.java
@@ -0,0 +1,14 @@
+package com.sky.controller.admin;
+
+import com.sky.controller.admin.おとうさん;
+
+public class son extends おとうさん {
+    public static void main(String[] args) {
+       son liu =new son();
+       liu.name="liu";
+        System.out.println(name);
+    }   
+    
+
+    
+}
---
+++ sky-server/src/main/java/com/sky/controller/admin/おとうさん.java
@@ -0,0 +1,8 @@
+package com.sky.controller.admin;
+
+public class おとうさん {
+    static String name;
+    int age;
+    String job;
+    String hobby;
+}
